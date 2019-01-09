78cd00752a9f6df94fa630dae0dcac3d78cd59c5
Adam Soffer <ads1018@gmail.com>
2019-01-09 17:36:59 -0500

    remove navigator

--- src/DefaultPlayer/DefaultPlayer.js
+++ src/DefaultPlayer/DefaultPlayer.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import videoConnect from './../video/video';
-import copy from './copy';
+import React from 'react'
+import PropTypes from 'prop-types'
+import videoConnect from './../video/video'
+import copy from './copy'
 import {
     setVolume,
     showTrack,
@@ -12,15 +12,15 @@ import {
     toggleFullscreen,
     getPercentagePlayed,
     getPercentageBuffered
-} from './../video/api';
-import styles from './DefaultPlayer.css';
-import Time from './Time/Time';
-import Seek from './Seek/Seek';
-import Volume from './Volume/Volume';
-import Captions from './Captions/Captions';
-import PlayPause from './PlayPause/PlayPause';
-import Fullscreen from './Fullscreen/Fullscreen';
-import Overlay from './Overlay/Overlay';
+} from './../video/api'
+import styles from './DefaultPlayer.css'
+import Time from './Time/Time'
+import Seek from './Seek/Seek'
+import Volume from './Volume/Volume'
+import Captions from './Captions/Captions'
+import PlayPause from './PlayPause/PlayPause'
+import Fullscreen from './Fullscreen/Fullscreen'
+import Overlay from './Overlay/Overlay'
 
 const DefaultPlayer = ({
     copy,
@@ -39,88 +39,99 @@ const DefaultPlayer = ({
     ...restProps
 }) => {
     return (
-        <div className={[
-            styles.component,
-            className
-        ].join(' ')}
-        style={style}>
-            <video
-                className={styles.video}
-                {...restProps}>
-                { children }
+        <div className={[styles.component, className].join(' ')} style={style}>
+            <video className={styles.video} {...restProps}>
+                {children}
             </video>
-            <Overlay
-                onClick={onPlayPauseClick}
-                {...video} />
-            { controls && controls.length && !video.error
-                ? <div className={styles.controls}>
-                        { controls.map((control, i) => {
-                            switch (control) {
-                                case 'Seek':
-                                    return <Seek
+            <Overlay onClick={onPlayPauseClick} {...video} />
+            {controls && controls.length && !video.error ? (
+                <div className={styles.controls}>
+                    {controls.map((control, i) => {
+                        switch (control) {
+                            case 'Seek':
+                                return (
+                                    <Seek
                                         key={i}
                                         ariaLabel={copy.seek}
                                         className={styles.seek}
                                         onChange={onSeekChange}
-                                        {...video} />;
-                                case 'PlayPause':
-                                    return <PlayPause
+                                        {...video}
+                                    />
+                                )
+                            case 'PlayPause':
+                                return (
+                                    <PlayPause
                                         key={i}
                                         ariaLabelPlay={copy.play}
                                         ariaLabelPause={copy.pause}
                                         onClick={onPlayPauseClick}
-                                        {...video} />;
-                                case 'Fullscreen':
-                                    return <Fullscreen
+                                        {...video}
+                                    />
+                                )
+                            case 'Fullscreen':
+                                return (
+                                    <Fullscreen
                                         key={i}
                                         ariaLabel={copy.fullscreen}
                                         onClick={onFullscreenClick}
-                                        {...video} />;
-                                case 'Time':
-                                    return <Time
-                                        key={i}
-                                        {...video} />;
-                                case 'Volume':
-                                    return <Volume
+                                        {...video}
+                                    />
+                                )
+                            case 'Time':
+                                return <Time key={i} {...video} />
+                            case 'Volume':
+                                return (
+                                    <Volume
                                         key={i}
                                         onClick={onVolumeClick}
                                         onChange={onVolumeChange}
                                         ariaLabelMute={copy.mute}
                                         ariaLabelUnmute={copy.unmute}
                                         ariaLabelVolume={copy.volume}
-                                        {...video} />;
-                                case 'Captions':
-                                    return video.textTracks && video.textTracks.length
-                                        ? <Captions
-                                            key={i}
-                                            onClick={onCaptionsClick}
-                                            ariaLabel={copy.captions}
-                                            onItemClick={onCaptionsItemClick}
-                                            {...video}/>
-                                        : null;
-                                default:
-                                    return null;
-                            }
-                        }) }
-                    </div>
-                : null }
+                                        {...video}
+                                    />
+                                )
+                            case 'Captions':
+                                return video.textTracks &&
+                                    video.textTracks.length ? (
+                                    <Captions
+                                        key={i}
+                                        onClick={onCaptionsClick}
+                                        ariaLabel={copy.captions}
+                                        onItemClick={onCaptionsItemClick}
+                                        {...video}
+                                    />
+                                ) : null
+                            default:
+                                return null
+                        }
+                    })}
+                </div>
+            ) : null}
         </div>
-    );
-};
+    )
+}
 
-const controls = ['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen', 'Captions'];
+const controls = [
+    'PlayPause',
+    'Seek',
+    'Time',
+    'Volume',
+    'Fullscreen',
+    'Captions'
+]
 
 DefaultPlayer.defaultProps = {
     copy,
     controls,
     video: {}
-};
+}
 
 DefaultPlayer.propTypes = {
     copy: PropTypes.object.isRequired,
     controls: PropTypes.arrayOf(PropTypes.oneOf(controls)),
     video: PropTypes.object.isRequired
-};
+}
 
 const connectedPlayer = videoConnect(
     DefaultPlayer,
@@ -132,7 +143,7 @@ const connectedPlayer = videoConnect(
             // TODO: This is not pretty. Doing device detection to remove
             // spinner on iOS devices for a quick and dirty win. We should see if
             // we can use the same readyState check safely across all browsers.
-            loading: readyState < (/iPad|iPhone|iPod/.test(navigator.userAgent) ? 1 : 4),
+            loading: false,
             percentagePlayed: getPercentagePlayed(restState),
             percentageBuffered: getPercentageBuffered(restState),
             ...restState
@@ -143,11 +154,16 @@ const connectedPlayer = videoConnect(
         onVolumeClick: () => toggleMute(videoEl, state),
         onCaptionsClick: () => toggleTracks(state),
         onPlayPauseClick: () => togglePause(videoEl, state),
-        onCaptionsItemClick: (track) => showTrack(state, track),
-        onVolumeChange: (e) => setVolume(videoEl, state, e.target.value),
-        onSeekChange: (e) => setCurrentTime(videoEl, state, e.target.value * state.duration / 100)
+        onCaptionsItemClick: track => showTrack(state, track),
+        onVolumeChange: e => setVolume(videoEl, state, e.target.value),
+        onSeekChange: e =>
+            setCurrentTime(
+                videoEl,
+                state,
+                (e.target.value * state.duration) / 100
+            )
     })
-);
+)
 
 export {
     connectedPlayer as default,
@@ -159,4 +175,4 @@ export {
     PlayPause,
     Fullscreen,
     Overlay
-};
+}
